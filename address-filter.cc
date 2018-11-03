f414a0e0cf97d6de8476b23051a81bdaa1fe602a
Derrick Pallas <derrick@pallas.us>
2018-11-03 09:19:29 -0700

    fix incorrect sizeof wrt inet_aton, update tests

--- address-filter.cc
+++ address-filter.cc
@@ -14,11 +14,11 @@ int main(int argc, char *argv[]) {
     while (std::cin >> s) {
         if (1 == inet_pton(AF_INET, s.c_str(), &ia)) {
             char buf[INET_ADDRSTRLEN];
-            if (inet_ntop(AF_INET, &ia, buf, sizeof ia))
+            if (inet_ntop(AF_INET, &ia, buf, sizeof buf))
                 std::cout << buf << std::endl;
         } else if (1 == inet_pton(AF_INET6, s.c_str(), &ia)) {
             char buf[INET6_ADDRSTRLEN];
-            if (inet_ntop(AF_INET6, &ia, buf, sizeof ia))
+            if (inet_ntop(AF_INET6, &ia, buf, sizeof buf))
                 std::cout << buf << std::endl;
         }
     }
